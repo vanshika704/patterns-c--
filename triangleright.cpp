3eaf416cf6295885dd3b0cc052f45f543f29af47
vanshika704 <sharmavanshi704@gmail.com>
2024-06-15 17:01:59 +0530

    ..

---
+++ triangleright.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 0; k <= i; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
